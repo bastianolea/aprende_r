ea55e4093737928d826e619fe3e65b1e50f4bbcc
bolea <bastian.olea@subdere.gov.cl>
2025-12-05 11:15:39 -0300

    hojas de apuntes

--- datos.R
+++ datos.R
@@ -61,3 +61,80 @@ paquetes <- list(
     descr = "Modelamiento estadístico, inferencia, machine learning y más gracias a una colección de paquetes"
   )
 )
+
+
+# https://raw.githubusercontent.com/rstudio/cheatsheets/main/translations/spanish/
+apuntes <- list(
+  intro = list(
+    imagen = "img/cheatsheets/introduccion-a-r.jpg",
+    link = "pdf/introduccion-a-r.pdf",
+    nombre = "Introducción a R",
+    descr = "Recordatorios básicos de conceptos principales"
+  ),
+  base = list(
+    imagen = "img/cheatsheets/base-r_es.jpg",
+    link = "pdf/base-r_es.pdf",
+    nombre = "Base R",
+    descr = "Conceptos básicos para usar R en la práctica"
+  ),
+  syntax_es = list(
+    imagen = "img/cheatsheets/rstudio-ide_es.jpg",
+    link = "pdf/rstudio-ide_es.pdf",
+    nombre = "RStudio",
+    descr = "Aspectos principales del entorno de desarrollo RStudio"
+  ),
+  dplyr = list(
+    imagen = "img/cheatsheets/data-transformation_es.jpg",
+    link = "pdf/data-transformation_es.pdf",
+    nombre = "Dplyr",
+    descr = "Transformación y manipulación de datos con `dplyr`"
+  ),
+  datos = list(
+    imagen = "img/cheatsheets/data-import_es.jpg",
+    link = "pdf/data-import_es.pdf",
+    nombre = "Importar datos",
+    descr = "Carga datos desde CSV, Excel y Google Drive"
+  ),
+  descript = list(
+    imagen = "img/cheatsheets/estadistica-descriptiva-con-R.jpg",
+    link = "pdf/estadistica-descriptiva-con-R.pdf",
+    nombre = "Estadísticas descriptivas",
+    descr = "Estadísticas descriptivas básicas"
+  ),
+  ggplot = list(
+    imagen = "img/cheatsheets/data-visualization_es.jpg",
+    link = "pdf/data-visualization_es.pdf",
+    nombre = "Visualización",
+    descr = "Visualización de datos con `ggplot2`"
+  ),
+  syntax_es = list(
+    imagen = "img/cheatsheets/syntax_es.jpg",
+    link = "pdf/syntax_es.pdf",
+    nombre = "Sintaxis de R",
+    descr = "Compara distintas formas de hacer lo mismo"
+  ),
+  forcats = list(
+    imagen = "img/cheatsheets/factors_es.jpg",
+    link = "pdf/factors_es.pdf",
+    nombre = "Factores",
+    descr = "Trabaja con datos en formato factor con `forcats`"
+  ),
+  stringr = list(
+    imagen = "img/cheatsheets/strings_es.jpg",
+    link = "pdf/strings_es.pdf",
+    nombre = "Texto",
+    descr = "Trabaja con datos en formato de texto con `stringr`"
+  ),
+  lubridate = list(
+    imagen = "img/cheatsheets/lubridate_es.jpg",
+    link = "pdf/lubridate_es.pdf",
+    nombre = "Fechas",
+    descr = "Trabaja con datos en formato fecha y hora con `lubridate`"
+  ),
+  shiny = list(
+    imagen = "img/cheatsheets/shiny_es.jpg",
+    link = "pdf/shiny_es.pdf",
+    nombre = "Shiny",
+    descr = "Desarrollo de aplicaciones web interactivas centradas en datos"
+  )
+)
